b0c6dc5aae67bb22c9a4897a17586072edd15ecd
yangshaoxiong <yangshaoxiong@boco.com.cn>
2017-05-15 15:11:14 +0800

    enable viewpager and bottom nagivation bar

--- app/src/main/java/com/example/navigationtest/MainActivity.java
+++ app/src/main/java/com/example/navigationtest/MainActivity.java
@@ -3,13 +3,30 @@ package com.example.navigationtest;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.example.navigationtest.fragment.Fragment1;
+import com.example.navigationtest.fragment.Fragment2;
+import com.example.navigationtest.fragment.Fragment3;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
+    ViewPager viewPager;
+
+    FragmentPagerAdapter adapter;
+
+    ArrayList<Fragment> fragments = new ArrayList<>();
+
     private TextView mTextMessage;
+
+    BottomNavigationView navigation;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -18,13 +35,16 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+//                    mTextMessage.setText(R.string.title_home);
+                    viewPager.setCurrentItem(0, true);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    viewPager.setCurrentItem(1, true);
+//                    mTextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    viewPager.setCurrentItem(2, true);
+//                    mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
             return false;
@@ -37,9 +57,61 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+//        mTextMessage = (TextView) findViewById(R.id.message);
+        navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        initViews();
+    }
+
+    private void initViews(){
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                switch (position) {
+                    case 0:
+                        navigation.setSelectedItemId(R.id.navigation_home);
+                        break;
+                    case 1:
+                        navigation.setSelectedItemId(R.id.navigation_dashboard);
+                        break;
+                    case 2:
+                        navigation.setSelectedItemId(R.id.navigation_notifications);
+                        break;
+                }
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        fragments = new ArrayList<>();
+        fragments.add(new Fragment1());
+        fragments.add(new Fragment2());
+        fragments.add(new Fragment3());
+
+        adapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return fragments.get(position);
+            }
+
+            @Override
+            public int getCount() {
+                return fragments.size();
+            }
+        };
+
+        viewPager.setAdapter(adapter);
+
     }
 
 }
---
+++ app/src/main/java/com/example/navigationtest/fragment/Fragment3.java
@@ -0,0 +1,28 @@
+package com.example.navigationtest.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.navigationtest.R;
+
+/**
+ * Created by ysx on 2017/5/15.
+ */
+
+public class Fragment3 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = LayoutInflater.from(getActivity()).inflate(R.layout.fragment, null);
+
+        TextView txt = (TextView) view.findViewById(R.id.txt);
+        txt.setText(this.getClass().getSimpleName());
+
+        return view;
+    }
+}
